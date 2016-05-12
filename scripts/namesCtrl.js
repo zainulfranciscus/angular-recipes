87df6106f1da3034ca5d2707512752ca069fb75d
Zainul Franciscus <zainul.franciscus@ge.com>
2016-05-12 15:59:07 +1000

    implement custom filters

--- scripts/namesCtrl.js
+++ scripts/namesCtrl.js
@@ -1,8 +1,8 @@
-angular.module('myApp', []).controller('namesCtrl', function($scope) {
+var app = angular.module('myApp', []).controller('namesCtrl', function($scope) {
     $scope.names = [
-        {name:'Sam',country:'America',currency:'USD',income:100000},
-        {name:'Bill',country:'Britain',currency:'GBP',income:150000},
-        {name:'Tran',country:'Thailand',currency:'THB',income:200000}
+        {name:'sam',country:'America',currency:'USD',income:100000},
+        {name:'bill',country:'Britain',currency:'GBP',income:150000},
+        {name:'tran',country:'Thailand',currency:'THB',income:200000}
     ];
 
     $scope.orderByMe= function(orderBy) {
@@ -11,3 +11,12 @@ angular.module('myApp', []).controller('namesCtrl', function($scope) {
 
     };
 });
+
+
+app.filter('capitalizeFirstLetter', function(){
+
+	return function(text) {
+		return text.charAt(0).toUpperCase() + text.slice(1);
+	};	
+
+})
