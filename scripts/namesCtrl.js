6d8debfb1b8ac8bf34bc26c01e688fb1f66ede84
Zainul Franciscus <zainul.franciscus@ge.com>
2016-05-12 15:34:45 +1000

    add examples about filter and scopes

--- scripts/namesCtrl.js
+++ scripts/namesCtrl.js
@@ -1,7 +1,7 @@
 angular.module('myApp', []).controller('namesCtrl', function($scope) {
     $scope.names = [
-        {name:'Jani',country:'Norway'},
-        {name:'Hege',country:'Sweden'},
-        {name:'Kai',country:'Denmark'}
+        {name:'Sam',country:'America',currency:'USD',income:100000.150},
+        {name:'Bill',country:'Britain',currency:'GBP',income:150000.750},
+        {name:'Tran',country:'Thailand',currency:'THB',income:200000.800}
     ];
 });
